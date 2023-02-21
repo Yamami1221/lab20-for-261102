b8ad015442aad2f9aa9d62704be4b6ef606644e8
Yamami1221 <anak2547@gmail.com>
2023-02-21 15:56:25 +0700

    lab20_1.cpp completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,10 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1, Rect r2){
+	double ow, oh;
+	ow = min(r1.x+r1.w, r2.x+r2.w) - max(r1.x, r2.x);
+	oh = min(r1.y, r2.y) - max(r1.y-r1.h, r2.y-r2.h);
+	if(ow>0 && oh>0) return ow*oh;
+	else return 0;
 }
